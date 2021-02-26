20dbe92459cd60f219998e945cd19c13d73e0d8d
OHRassi <paul@paulbenderdev.com>
2021-02-26 08:58:16 -0500

    another day, more codewars...

---
+++ 2021 - 02 - 26 codewars/7kyu - String Drills - Repeater.js
@@ -0,0 +1,18 @@
+// Instructions: 
+
+// Write a function named repeater() that takes two arguments (a string and a number), and returns a new string where the input string is repeated that many times.
+
+// Example:
+// Repeater.repeat("a", 5)
+// should return
+
+// "aaaaa"
+
+function repeater(string, n){
+  let newString = "";
+  newString = newString + string;
+  while((newString.length / string.length) < n) {
+    newString = newString + string;
+  }
+  return newString;
+}
