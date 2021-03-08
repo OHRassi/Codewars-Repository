668be1329147e89f6ee497a9168a003106d29c28
OHRassi <paul@paulbenderdev.com>
2021-03-08 11:14:16 -0500

    another day, more codewars...

---
+++ 8kyu/21-03-08 - 8kyu - How good are you really.js
@@ -0,0 +1,19 @@
+// Instructions:
+
+// There was a test in your class and you passed it. Congratulations!
+// But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+// You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+// Return True if you're better, else False!
+
+// Note:
+// Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!
+
+function betterThanAverage(classPoints, yourPoints) {
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+  let totalPoints = (classPoints.reduce(reducer));
+  totalPoints = totalPoints + yourPoints
+  return (totalPoints / (classPoints.length + 1 )) < yourPoints
+}
+
